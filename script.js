9a2d5df19128d0e8fe000afaac1d48f0e1b19ec3
Samantha Tarrice <s.tarrice@gmail.com>
2021-10-18 14:49:21 -0300

    add cursor to links and start work page

--- script.js
+++ script.js
@@ -10,4 +10,6 @@ window.addEventListener('load', function () {
   dropDown.classList.add('hide');
 })
 
-// How do I make the menu go back up when another part of the page is clicked
+// How do I make the menu go back up when another part of the page is clicked.
+
+// What's the best way to add js for specific pages? For example, I want to change the navbar to not be fixed on all pages except the home page. Would it be best to just add a new script.js file just for that page? Or, would I be able to add something that targets specific pages?
